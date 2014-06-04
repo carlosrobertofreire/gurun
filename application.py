5e5c5f24fedae4bf06111274d1e92d335130e50f
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2014-06-03 22:18:26 -0300

    Primeira utilização do index.html com estilo bootstrap

--- application.py
+++ application.py
@@ -1,14 +1,15 @@
-import flask
+from flask import Flask
+from flask import render_template
 
-application = flask.Flask(__name__)
+application = Flask(__name__)
 
 #Set application.debug=true to enable tracebacks on Beanstalk log output.
 #Make sure to remove this line before deploying to production.
 application.debug=True
 
 @application.route('/')
-def home():
-    return "Home!"
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', debug=False)
+    application.run(host='0.0.0.0', debug=True)
