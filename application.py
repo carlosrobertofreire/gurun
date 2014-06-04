2adae625490da4e826db4a072819b321edfd610e
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2014-06-04 18:52:39 -0300

    Constução inicial de código para criação de tabelas no DynamoDB, ainda sem testes.

--- application.py
+++ application.py
@@ -3,6 +3,8 @@ from flask import render_template
 from flask import request
 from models import Analise, Numerologia
 
+from dynamodb_mapper.model import ConnectionBorg
+
 application = Flask(__name__)
 
 #Set application.debug=true to enable tracebacks on Beanstalk log output.
@@ -14,10 +16,19 @@ def index():
     if request.method == 'POST':
         nome = request.form['nome']
         analise = Numerologia.analisar(nome)
-        print "Nome %s analisado com sucesso! Soma %d - Resultado %s" % (analise.nome, analise.valor, analise.resultado)
+        analise.save()
         return render_template('index.html', analise = analise)
     else:
         return render_template('index.html')
 
 if __name__ == '__main__':
+    try:
+        print "Criando tabela..."
+        conn = ConnectionBorg()
+        conn.set_region("sa-east-1")
+        conn.create_table(Analise, 1, 1, wait_for_active=True)
+        print "Tabela criada!"
+    except:
+        print "Erro durante criacao de tabela!"
+        pass
     application.run(host='0.0.0.0', debug=True)
