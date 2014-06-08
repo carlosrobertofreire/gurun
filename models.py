e2473df4386ce7ee1e68c31447cf0a4f0ef8ed07
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2014-06-08 19:16:06 -0300

    Acentuação retirada do nome dentro da função de análise. Fix #8

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from dynamodb_mapper.model import DynamoDBModel, utc_tz
 from datetime import datetime
 import pytz
+from unicodedata import normalize
 
 local_tz = pytz.timezone('America/Sao_Paulo')
 
@@ -8,6 +9,9 @@ def utc_to_local(utc_dt):
     local_dt = utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
     print local_tz.normalize(local_dt)
     return local_tz.normalize(local_dt)
+
+def remover_acentos(txt):
+    return normalize('NFKD', txt).encode('ASCII','ignore')
 
 class Analise(DynamoDBModel):
     __table__ = u"numerologia-ws-analises"
@@ -25,8 +29,9 @@ class Numerologia:
     def analisar(nome):
 	if not nome:
             raise Exception('Nome invalido!')
+        nomeSemAcentos = remover_acentos(nome)
         dic = {'A':1,'B':2,'C':3,'D':4,'E':5,'F':6,'G':7,'H':8,'I':9,'J':9,'K':10,'L':20,'M':30,'N':40,'O':50,'P':60,'Q':70,'R':80,'S':90,'T':100,'U':200,'V':200,'W':200,'X':300,'Y':300,'Z':400}
-        letras = list(filter(lambda c: c.isalpha(), nome) )
+        letras = list(filter(lambda c: c.isalpha(), nomeSemAcentos) )
         soma = 0
         for letra in letras:
             if not letra:
