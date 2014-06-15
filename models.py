ec7bc4a66f8c97b8a058ef5197842131fe006126
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2014-06-14 23:16:53 -0300

    Acentuação na exceção.

--- models.py
+++ models.py
@@ -30,7 +30,7 @@ class Numerologia:
     @staticmethod
     def analisar(nome):
 	if not nome:
-            raise Exception('Nome invalido!')
+            raise Exception('Nome inválido!')
         nomeSemAcentos = remover_acentos(nome)
         dic = {'A':1,'B':2,'C':3,'D':4,'E':5,'F':6,'G':7,'H':8,'I':9,'J':9,'K':10,'L':20,'M':30,'N':40,'O':50,'P':60,'Q':70,'R':80,'S':90,'T':100,'U':200,'V':200,'W':200,'X':300,'Y':300,'Z':400}
         letras = list(filter(lambda c: c.isalpha(), nomeSemAcentos) )
