a395481a4424c901b6c8f9a9a4f3ba6ac32b9160
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2014-07-28 22:20:14 -0300

    Funções utilitárias movidas para util.py

--- models.py
+++ models.py
@@ -1,20 +1,6 @@
 # coding=UTF-8
 from dynamodb_mapper.model import DynamoDBModel
 from datetime import datetime
-import pytz
-from unicodedata import normalize
-
-local_tz = pytz.timezone('America/Sao_Paulo')
-
-
-def utc_to_local(utc_dt):
-    local_dt = utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
-    print local_tz.normalize(local_dt)
-    return local_tz.normalize(local_dt)
-
-
-def remover_acentos(txt):
-    return normalize('NFKD', txt).encode('ASCII', 'ignore')
 
 
 class Analise(DynamoDBModel):
