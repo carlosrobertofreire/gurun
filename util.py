5f0215e38b672a5df47de875399833f8505773f1
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2014-08-22 15:15:14 -0300

    Função util para remover caracteres especiais de uma String.

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 # coding=UTF-8
 import pytz
+import re
 from unicodedata import normalize
 
 local_tz = pytz.timezone('America/Sao_Paulo')
@@ -13,3 +14,7 @@ def utc_to_local(utc_dt):
 
 def remover_acentos(txt):
     return normalize('NFKD', txt).encode('ASCII', 'ignore')
+
+
+def remover_caracteres_especiais(txt):
+    return re.sub('[^A-Za-zÀ-ú]+', ' ', txt)
