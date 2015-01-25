e207194c0ba5136af524a13f755f5bc2147c80a9
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2015-01-25 21:35:11 -0200

    Adaptação inicial de fluxo utilizando componentes Angular (dados fake).

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,23 @@
  * # MainCtrl
  * Controller of the numerologiaWsApp
  */
-angular.module('numerologiaWsApp')
-  .controller('MainCtrl', function ($scope) {
+angular.module('numerologiaWsApp').controller('MainCtrl', function($scope) {
+
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
     ];
-  });
+
+    $scope.resultList = [];
+
+    $scope.analyze = function() {
+        $scope.resultList.push({
+            'text': this.name,
+            'number': 3,
+            'value': 'EXCELENTE'
+        })
+    };
+
+
+});
