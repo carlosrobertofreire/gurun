e30ba6cad000421f13bf673bfa02eaeab4965731
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2015-01-25 21:36:42 -0200

    Exclusão de dados de teste do angular-generator.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,12 +9,6 @@
  */
 angular.module('numerologiaWsApp').controller('MainCtrl', function($scope) {
 
-    $scope.awesomeThings = [
-        'HTML5 Boilerplate',
-        'AngularJS',
-        'Karma'
-    ];
-
     $scope.resultList = [];
 
     $scope.analyze = function() {
