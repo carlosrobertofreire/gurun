88dd5be1cfef8a9a69dbe76440075116371f1e54
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2015-01-25 22:32:16 -0200

    Diretiva para letras maiúsculas em um input.

---
+++ app/scripts/directives/capitalize.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name numerologiaWsApp.directive:capitalize
+ * @description
+ * # capitalize
+ */
+angular.module('numerologiaWsApp').directive('capitalize', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, modelCtrl) {
+            var capitalize = function(inputValue) {
+                if (inputValue == undefined) inputValue = '';
+                var capitalized = inputValue.toUpperCase();
+                if (capitalized !== inputValue) {
+                    modelCtrl.$setViewValue(capitalized);
+                    modelCtrl.$render();
+                }
+                return capitalized;
+            }
+            modelCtrl.$parsers.push(capitalize);
+            capitalize(scope[attrs.ngModel]);
+        }
+    };
+});
