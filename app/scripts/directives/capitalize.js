03b65f9310f30275439c453b93c3fc78e6feb048
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2015-02-01 20:42:00 -0200

    Recomendações jshint.

--- app/scripts/directives/capitalize.js
+++ app/scripts/directives/capitalize.js
@@ -11,14 +11,16 @@ angular.module('numerologiaWsApp').directive('capitalize', function() {
         require: 'ngModel',
         link: function(scope, element, attrs, modelCtrl) {
             var capitalize = function(inputValue) {
-                if (inputValue == undefined) inputValue = '';
+                if (inputValue === undefined) {
+                    inputValue = '';
+                }
                 var capitalized = inputValue.toUpperCase();
                 if (capitalized !== inputValue) {
                     modelCtrl.$setViewValue(capitalized);
                     modelCtrl.$render();
                 }
                 return capitalized;
-            }
+            };
             modelCtrl.$parsers.push(capitalize);
             capitalize(scope[attrs.ngModel]);
         }
